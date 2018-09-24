ba143ec67b5e1b2b8f5004d8bfa6d3a04bb75ad2
Olivia Ledford <dev.oledford@gmail.com>
2018-09-23 22:58:36 -0500

    Implement basic application structure

---
+++ src/components/_theme.js
@@ -0,0 +1,11 @@
+import { COLOR } from 'react-native-material-ui';
+
+export const MENU_BAR_COLOR = COLOR.amber400;
+
+const uiTheme = {
+  palette: {
+    primaryColor: MENU_BAR_COLOR,
+  },
+};
+
+export default uiTheme;
