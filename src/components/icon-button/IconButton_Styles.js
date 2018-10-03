0979df27222e4195c4b41686c252793f5da9961d
Dylan Klohr <dylan.sftwr@gmail.com>
2018-10-03 07:24:37 -0500

    nuke original project setup

--- src/components/icon-button/IconButton_Styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-const IconButtonStyles = StyleSheet.create({
-  test: {
-    backgroundColor: 'black',
-  }
-});
-
-export default IconButtonStyles;
--- src/components/toolbar/ToolbarSocket/_toolbarSocketPropTypes.js
+++
@@ -1,6 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const toolbarSocketPropTypes = PropTypes.oneOfType([
-  PropTypes.node,
-  PropTypes.arrayOf(PropTypes.node),
-]);
