c39a6ceb611e8ea451cd882846d55b5baeeff01b
Olivia Ledford <onlpp9@mail.missouri.edu>
2018-12-02 11:40:30 -0600

    Prod build (#24)
    * set prod API_URL
    
    * built project MVP

---
+++ build/precache-manifest.41d9578cb0e521789a4e4d7d9e92ba4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "323dbb4a6dc3726f46a5",
+    "url": "/static/css/main.353d452a.chunk.css"
+  },
+  {
+    "revision": "323dbb4a6dc3726f46a5",
+    "url": "/static/js/main.323dbb4a.chunk.js"
+  },
+  {
+    "revision": "6b156dc29e08fca4ba4d",
+    "url": "/static/js/1.6b156dc2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "acb6057649d5f6361ae6e9de4022404a",
+    "url": "/index.html"
+  }
+];
