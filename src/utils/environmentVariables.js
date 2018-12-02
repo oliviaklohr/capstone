24519013d372381defce605e61aabd54c88d0dea
Dylan Klohr <dylan.sftwr@gmail.com>
2018-12-02 11:17:13 -0600

    set prod API_URL

--- src/utils/environmentVariables.js
+++ src/utils/environmentVariables.js
@@ -1,6 +1,6 @@
 const variableDefaults = {
-  // API_URL: 'http://localhost:8080',
-  API_URL: 'http://192.168.1.166:8080',
+  // API_URL: 'http://localhost:8080', // only used for local development
+  API_URL: 'http://ec2-52-202-10-230.compute-1.amazonaws.com:8080',
 };
 
 const developmentEnvirons = {
